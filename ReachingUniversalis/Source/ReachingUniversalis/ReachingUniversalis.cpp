251c324e7bf0badf7bf8fd4a890a2f424b423cda
Kasey Keahey <Kasey_Keahey@yahoo.com>
2026-04-08 20:26:35 +0200

    returning to start

--- ReachingUniversalis/Source/ReachingUniversalis/ReachingUniversalis.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ReachingUniversalis.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReachingUniversalis, "ReachingUniversalis" );
